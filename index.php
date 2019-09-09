52f91944e0e40c804335ecd85b23548c95fc3282
dotsbik <dacun@kartazamerov.ru>
2019-09-09 17:08:49 +0300

    Голая структура

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+  <head>
+    <title>Tennis club Phoenix</title>
+    <link href="css/style.css" type="text/css" rel="stylesheet" />
+
+  </head>
+
+
+
+<body>
+  <div id="wrapper">
+    <div id="header">
+      <div class="top_title"></div>
+      <div class="slide"></div>
+      <div class="menu"></div>
+
+    </div>
+    <div id="content">
+      <div class="present"></div>
+      <div class="main"></div>
+      <div class="news"></div>
+
+
+
+
+    </div>
+    <div id="footer"></div>
+
+
+
+
+
+
+
+  </div>
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+</html>
